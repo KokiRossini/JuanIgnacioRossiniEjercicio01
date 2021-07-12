9279fdf64d6d30199324d9ab3fdb82f5335ef5ef
Juan Ignacio Rossini <koki-rossini@hotmail.com.ar>
2021-07-12 19:19:07 -0300

    Agregar archivos de proyecto.

---
+++ JuanIgnacioRossiniEjercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuanIgnacioRossiniEjercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double catetoA, catetoB;
+            double superficie, hipotenusa, perimetro;
+
+            Console.WriteLine("ingrese el primer cateto");
+            catetoA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo cateto");
+            catetoB = Convert.ToDouble(Console.ReadLine());
+
+            superficie = catetoA * catetoB / 2;
+            hipotenusa = Math.Sqrt(Math.Pow(catetoA,2)+Math.Pow(catetoB,2));
+            perimetro = catetoA + catetoB + hipotenusa;
+
+            Console.WriteLine($"la superficie del triangulo es {superficie}");
+            Console.WriteLine($"el perimetro del triangulo es {perimetro}");
+            Console.ReadLine();
+        }
+    }
+}
